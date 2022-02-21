9d3bbd790d497b44b07fbb771cd3e0ab92013683
Siddharth Roy <siddharthroy36912@gmail.com>
2022-02-21 16:14:20 +0530

    Section 12 done

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -25,6 +25,10 @@ export class RecipeService {
     return [ ...this.recipes ];
   }
 
+  getRecipe(id: number) {
+    return this.recipes[id];
+  }
+
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
   }
